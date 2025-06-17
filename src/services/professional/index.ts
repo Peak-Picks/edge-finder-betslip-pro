e829c04a392c56bb93a81ac21a9d0d9360ea30c8
Riggy16 <rrennat16@yahoo.com>
2025-06-17 02:27:08 -0500

    Create index.ts

---
+++ src/services/professional/index.ts
@@ -0,0 +1,5 @@
+export { calculationEngine } from './calculationEngine';
+export { dataManager } from './dataManager';
+export { professionalPicksService } from './picksService';
+export { wnbaModel, mlbModel } from './sportModels';
+export type { ProfessionalPick } from './picksService';
